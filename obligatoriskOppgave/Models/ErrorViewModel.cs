b5961564c590fafae481cf462c819b1d9e2f6d77
HuanLee1 <huanl.privat@gmail.com>
2025-09-24 14:17:20 +0200

    first commit

---
+++ obligatoriskOppgave/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace obligatoriskOppgave.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
